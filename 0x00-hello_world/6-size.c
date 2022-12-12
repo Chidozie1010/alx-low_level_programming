227665a3e9928fddee530ba4f7818605968af145
Chidozie1010 <chidoziellkanah@gmail.com>
2022-12-12 00:49:07 -0800

    task6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - runs data type
+ *
+ * Return: Always 0(Success)
+ */
+
+int main(void)
+{
+	char q;
+	int w;
+	long int e;
+	long long int r;
+	float f;
+
+	printf("size of a char: %lubyte(s)\n", (unsigned long)size of (q));
+	printf(size of an int: %lubyte(s)\n", (unsigned long) size of (w));
+	printf("size of a long int: %lubyte(s)\n", (unsigned long) size of (e));
+	printf("size of a long long int: %lubyte(s)\n", (unsigned long) size of (r));
+	printf("size of a float: %lubyte(s)\n", (unsigned long) size of (f));
+	Return (0);
+}
