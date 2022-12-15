c8ca39ceed3cfa31394c121fa9ef05139aefe87b
Chidozie1010 <chidoziellkanah@gmail.com>
2022-12-15 00:46:56 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the lower case alphabet
+ *
+ * Return: void
+ *
+ */
+
+void print_alphabet(void)
+
+{
+	char c = 'a';
+
+	int i;
+
+	for (i = 0; i < 26; i++)
+
+	{
+
+		_putchar(c + i);
+
+	}
+
+	_putchar(10);
+
+}
