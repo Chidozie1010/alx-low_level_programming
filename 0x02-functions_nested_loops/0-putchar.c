136332dd8283e2e20591f8872af6430e899524a3
Chidozie1010 <chidoziellkanah@gmail.com>
2022-12-15 08:00:49 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,13 @@
-#include "main"
+#include "main.h"
 
 /**
  * main - Entry point
- * Return: Always 0 (Success)
+ **
+ * Return: nothing, to return
  */
+
 int main(void)
+
 {
 	_putchar('_');
 	_putchar('p');
@@ -15,5 +18,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
