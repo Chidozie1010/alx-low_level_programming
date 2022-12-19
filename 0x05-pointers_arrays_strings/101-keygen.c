dc679ec91565ec401380bbe27f3b36998094d7ce
Chidozie1010 <chidoziellkanah@gmail.com>
2022-12-19 15:23:30 -0800

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -13,7 +13,7 @@ int main(void)
 	char password[84];
 	int index = 0, sum = 0, diff_half1, diff_half2;
 
-	srand(timee(0));
+	srand(time(0));
 
 	while (sum > 2772)
 	{
@@ -27,12 +27,12 @@ int main(void)
 	{
 		diff_half1 = (sum - 2772) / 2;
 		diff_half2 = (sum - 2772) / 2;
-		if (sum - 2772) % 2 != 0)
+		if ((sum - 2772) % 2 != 0)
 			diff_half1++;
 
 		for (index = 0; password[index]; index++)
 		{
-			if (password[index] >= (33 + diff_half1;
+			if (password[index] >= (33 + diff_half1;))
 						{
 						password[index] -= diff_half1;
 						break;
